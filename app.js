0e585b22eb6adedfe2e48e4217f9a5c34f86c087
Ammukutty Pradeep <84181961+ammupradeep@users.noreply.github.com>
2022-08-06 20:42:40 +0530

    Cesius -  Fahreheit Conversion added

--- app.js
+++ app.js
@@ -34,6 +34,28 @@ function setPosition(position){
     getWeather(latitude,longitude);
 }
 
+//C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 35;
+}
+
+//When user clicks on temperature element
+tempElement.addEventListener("click",function(){
+    if(weather.temperature.value === undefined) return;
+
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+})
+
+
 //Show error if any
 function showError(error){
     notificationElement.style.display = "block";

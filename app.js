1a1ba1aa8ef23dcd9e1c3b1f37c6060fed39a35f
Ammukutty Pradeep <ammumalikachirayil@gmail.com>
2022-08-06 20:00:00 +0530

    Website completed and added

---
+++ app.js
@@ -0,0 +1,71 @@
+//SELECT elements
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+//App data
+const weather = {};
+
+weather.temperature = {
+    unit : "celsius"
+}
+
+//App constants and vars
+const KELVIN = 273;
+
+const key = "82005d27a116c2880c8f0fcb866998a0";
+
+//Check if browser supported geolocation
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p> Browser doesn't Support Geolocation</p>";
+}
+
+//Set user's position
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    // console.log(longitude)
+
+    getWeather(latitude,longitude);
+}
+
+//Show error if any
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+//Get weather from API
+function getWeather(latitude,longitude){
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        })
+}
+
+//Display weather to UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+
+}
